dd2a286fc5f44db05b829176516ab585c98be81c
neroZWX <971370106@qq.com>
2018-03-30 01:58:32 +0100

    Update subl-sum.rkt

--- sublsum/subl-sum.rkt
+++ sublsum/subl-sum.rkt
@@ -1,7 +1,8 @@
 #lang racket
-(define (sum list)
-   (if (null? list)
+(define (sum list);first define a method as sum list, then set 1 check the list whether null or not
+   (if (null? list);then use 'apply'  the method to puls the list whether 0 adapt:https://stackoverflow.com/questions/39044742/example-of-apply-other-than-in-racket 
      #f
       (= (apply + list) 0) ))
-(define (sublsum list)
-  (filter sum (cdr (combinations list))) )
+(define (sublsum list);define sublsum list
+  (filter sum (cdr (combinations list))) );use filter to display a list which is true(euqal 0)
+  ;adapt from https://docs.racket-lang.org/reference/pairs.html#%28def._%28%28lib._racket%2Flist..rkt%29._combinations%29%29
